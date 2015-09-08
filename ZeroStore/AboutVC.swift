6fffd6c4e9745cc8192640746d65e7bd1be2a315
Kyle Bashour <kylebshr@me.com>
2015-09-07 20:20:59 -0400

    Add about page

---
+++ ZeroStore/AboutVC.swift
@@ -0,0 +1,59 @@
+//
+//  AboutVC.swift
+//  ZeroStore
+//
+//  Created by Kyle Bashour on 9/7/15.
+//  Copyright © 2015 Kyle Bashour. All rights reserved.
+//
+
+import UIKit
+import TTTAttributedLabel
+
+class AboutVC: UITableViewController {
+
+    @IBOutlet weak var twitterLinkLabel: UILabel!
+    @IBOutlet weak var versionLabel: UILabel!
+
+    let websiteURL = NSURL(string: "http://kylebashour.com")!
+    let twitterURL = NSURL(string: "twitter://user?screen_name=kylebshr")!
+    let twitterWebsiteURL = NSURL(string: "http://twitter.com/kylebshr")!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setVersionNumber()
+    }
+
+    @IBAction func doneButtonPressed(sender: UIBarButtonItem) {
+        presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func setVersionNumber() {
+
+        if let
+            versionObject = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"],
+            versionString = versionObject as? String,
+            buildObject = NSBundle.mainBundle().infoDictionary?["CFBundleVersion"],
+            buildString = buildObject as? String
+        {
+            versionLabel.text = "\(versionString) build \(buildString)"
+        }
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+        if indexPath.section == 1 {
+            if indexPath.row == 0 {
+                UIApplication.sharedApplication().openURL(websiteURL)
+            }
+            else if indexPath.row == 1 {
+                if !UIApplication.sharedApplication().openURL(twitterURL) {
+                    UIApplication.sharedApplication().openURL(twitterWebsiteURL)
+                }
+            }
+
+        }
+    }
+}
